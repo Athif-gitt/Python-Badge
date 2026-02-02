f743576f44c6f46466bb6e579c81f7c934a61920
Athif-gitt <athiffaisal55@gmail.com>
2026-02-02 19:13:48 +0530

    aaa

--- tests.py
+++ tests.py
@@ -22,11 +22,59 @@
 # x.append(4)
 # print(id(x))
 
-a = 10
-b = 4
+# a = 10
+# b = 4
 
 # print(a & b)
 # print(a | b)
 # print(~a)
 # print(a ^ b)
+
+# ternary operator
+# a, b = 10, 20
+# min = a if a < b else b
+# print(min)
+
+# conditional statements
+# age = 1
+# if age >= 18:
+#     print("Eligible")
+# print("Sorry Not Eligibe")
+
+# age = 25
+# if age == 11:
+#     print("child")
+# elif(age == 19):
+#     print("teenager")
+# elif(age == 25):
+#     print("adult")
+
+# number = 2
+
+# match number:
+#     case 1:
+#         print("one")
+#     case 2 | 3 :
+#         print("two or three")
+#     case _:
+#         print("unkown number")
+
+# numb = 0
+# while numb <= 3:
+#     print(numb)
+#     numb +=1
+
+# while (True):
+#     print("Hello World")
+
+# a = 'Athif FAisal'
+# for letter in a:
+#     pass
+# print(letter)
+
+
+
+
+
+
 
