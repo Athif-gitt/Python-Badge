7dcb9a81d9162fee2e1f0c8521971502d02c9f0c
Athif-gitt <athiffaisal55@gmail.com>
2026-02-03 10:55:47 +0530

    aaa

--- tests.py
+++ tests.py
@@ -72,6 +72,100 @@
 #     pass
 # print(letter)
 
+# a = [1, 2, 3]
+# a.extend([4, 5])
+# print(a)
+
+
+# a = [1, 2, 3]
+# a.append([4, 5])
+# print(a)
+
+# l = [1, 2, 3]
+# a = l[::-1]
+# print(a)
+
+# t1 = (4,)
+# print(type(t1))
+
+# t = (1, 2, 3)
+# a, b, c = t
+# print(a)
+
+# t = (1, 2, 3)
+# print(id(t))
+# t1 = (4,)
+# t += t1
+# print(id(t))
+# print(t)
+
+# l = (1, 2, 3, 4,5 ,6 )
+# a, *b, c = l
+# print(*b)
+
+# emptry_set = set()
+# print(emptry_set)
+# emptry_set.add((1, 2, 3))
+# print(emptry_set)
+
+# a = {"name": 'Athif', "age": 21}
+
+# # print(a["class"])
+# a["c"] = 3
+# print(a)
+
+# lst = [1, 2, 2, 3]
+# freq = {}
+# for i in lst:
+#     freq[i] = freq.get(i, 0) + 1
+
+# print(freq)
+
+# freq = {}
+# for i in lst:
+#     freq[i] = lst.count(i)
+# print(freq)
+
+# squares = {}
+# for i in range(1, 6):
+#     squares[i] = i * i
+
+# squares = {i : i*i for i in range(1, 6)}
+
+# print (squares)
+
+# words = ["python", "java", "c"]
+
+# w_l = {word: len(word) for word in words}
+# print(w_l)
+
+# result = {x: "even" if x %2 == 0 else "odd" for x in range(1, 11)}
+# print(result)
+
+# result = {x: "odd"  for x in range(1, 11) if x % 2 != 0}
+# print(result)
+
+# class Dog:
+#     sound = "Bark"
+# # dog1 = Dog() # Here dog 1 is the object created using the class Dog...
+# # print(dog1.sound)
+
+#     def __init__(self, name, type):
+#         self.name = name
+#         self.type = type
+
+# dog1 = Dog("Toomy", "Canine")
+# print(dog1.type)
+
+
+
+
+
+
+
+
+    
+
 
 
 
